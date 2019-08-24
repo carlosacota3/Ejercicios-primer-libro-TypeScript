a7e286dd44136be1665751b08c591203a1dc3f24
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-08-24 14:49:12 -0600

    Interfaces

--- type.ts
+++
@@ -1,7 +0,0 @@
-function area(shape: string, width: number, height: number)
-{
-    var area = width * height;
-    return "I´m a " + shape + " with an area of " + area + " cm squared.";
-}
-
-document.body.innerHTML = area("rectangle", 30, 15);
