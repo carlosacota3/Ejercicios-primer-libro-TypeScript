81efa91c7f3ec53e7125aaea34bcaf92520f8c71
Carlos Antonio Cota Soto <carlos_antonio_cota_soto@hotmail.com>
2019-08-27 03:12:45 -0600

    Funciones Genéricas Usando Restricciones (con extends, de una clase o interface)

--- generico.js
+++ generico.js
@@ -1,4 +1,8 @@
-function removeIt(theInput, theIt) {
-    var theRegex = new RegExp(theIt, "gi");
-    return theInput.replace(theRegex, '');
+function printName(theInput) {
+    console.log("My name is " + theInput.name);
 }
+var serena = {
+    name: 'Serena Williams',
+    profession: 'Tennis Player'
+};
+printName(serena);
